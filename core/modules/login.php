8141ccec984d111afe8acbad6b4d66362ff69f85
grotfang <ben@walkerbox.co.uk>
2013-06-27 01:47:26 +0100

    Login module functional
    No template added yet. Module handles logging in and registering with a
    correctly configured database.

--- core/modules/login.php
+++ core/modules/login.php
@@ -8,15 +8,37 @@ class mod_login {
 
     private static $page = null;
 
+    private static $actions = array( 'register', 'login' );
+
     static function run() {
 
         $page = new page();
 
-        
+        $action = get( 'action' );
+
+        if( empty( $action ) ) {
+            $action = false;
+        } else if( !v::in( $action, $actions ) ) {
+            $action = false;
+        } else {
+            $action = $action;
+        }
+
+        if( $action ) {
+            $page->return = self::$action();
+        }
 
         global $site;
 
         $site->page = $page;
 
+    }
+
+    static function login() {
+        return auth::login( 'game' );
+    }
+
+    static function register() {
+        return auth::register();
     }
 }
