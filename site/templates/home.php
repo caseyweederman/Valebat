0516ea1e9d84519415f0b0629d571a27aa18fa30
grotfang <ben@walkerbox.co.uk>
2013-06-27 01:21:43 +0100

    Minor edit to header and footer

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,11 +1,9 @@
-<?php
+<?php snippet( 'header' ); ?>
 
-snippet( 'header' );
-
-?>
         <h1>Heading</h1>
+
 <?php
 echo $site->page()->var();
 ?>
-    </body>
-</html>
+
+<?php snippet( 'footer' ); ?>
