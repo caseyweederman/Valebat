27fee703c3744b1966996f77673fc9cf18d5da12
grotfang <ben@walkerbox.co.uk>
2013-06-28 13:06:16 +0100

    Quick whitespace cleanup

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -3,7 +3,7 @@
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <title>Valebat</title>
-        <?php echo css('assets/css/cards.css'); ?>
-        <?php echo js('assets/js/cards.js'); ?>
+        <?php echo css( 'assets/css/cards.css' ); ?>
+        <?php echo js( 'assets/js/cards.js' ); ?>
     </head>
     <body>
