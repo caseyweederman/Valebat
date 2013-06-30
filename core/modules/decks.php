ab4e1a03d4212cd28bb3146879658d45e5f0a25c
grotfang <ben@walkerbox.co.uk>
2013-06-30 22:36:08 +0100

    Add empty decks module

---
+++ core/modules/decks.php
@@ -0,0 +1,21 @@
+<?php
+
+// direct access protection
+if( !defined( 'VALEBAT' ) )
+    die( 'Direct access is not allowed' );
+
+class mod_home {
+
+    private static $page = null;
+
+    static function run() {
+
+        $page = new page();
+
+        global $site;
+
+        $site->page = $page;
+
+    }
+
+}
