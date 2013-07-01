cc34eaef0ee13a61c35a8d1bf89ae58292805cc2
Gord Hill <caseyweederman@gmail.com>
2013-07-01 19:05:38 -0400

    All up to date now.

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -4,8 +4,9 @@
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <title>Valebat</title>
         <?php echo css( 'assets/css/cards.css' ); ?>
+        <?php echo css( 'assets/css/match.css' ); ?>
+        <?php echo css( 'http://fonts.googleapis.com/css?family=Uncial+Antiqua' ); ?>
         <?php echo js( '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js' ); ?>
         <?php echo js( 'assets/js/cards.js' ); ?>
-        <?php echo css( 'http://fonts.googleapis.com/css?family=Uncial+Antiqua' ); ?>
     </head>
     <body>
